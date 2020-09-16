a7de7975135f8aa09c0ad4aa0ff62f1e0c72609b
wilson <wilson@miyou.ubuntu>
2020-09-17 00:26:01 +0800

    faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8 的配置，但最后还是没有生成成功

--- start_train.sh
+++ start_train.sh
@@ -5,12 +5,14 @@ work_dir=$(cd "$(dirname "$0")"; pwd)
 echo $work_dir
 
 # 训练管道配置文件
-PIPELINE_CONFIG_PATH=/home/wilson/workstation/myTrafficTF2/models/centernet_hg104_1024x1024_coco17_tpu-32/pipeline.config
-# PIPELINE_CONFIG_PATH=/home/wilson/workstation/myTrafficTF2/models/faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8/pipeline.config
+# PIPELINE_CONFIG_PATH=/home/wilson/workstation/my_traffic_tf2/models/centernet_hg104_512x512_coco17_tpu-8/pipeline.config
+# PIPELINE_CONFIG_PATH=/home/wilson/workstation/my_traffic_tf2/models/centernet_hg104_1024x1024_coco17_tpu-32/pipeline.config
+PIPELINE_CONFIG_PATH=/home/wilson/workstation/my_traffic_tf2/models/faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8/pipeline.config
 
 # 模型训练的临时目录
-MODEL_DIR=/home/wilson/workstation/myTrafficTF2/models/centernet_hg104_1024x1024_coco17_tpu-32/build
-# MODEL_DIR=/home/wilson/workstation/myTrafficTF2/models/faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8/build
+# MODEL_DIR=/home/wilson/workstation/my_traffic_tf2/models/centernet_hg104_512x512_coco17_tpu-8/build
+# MODEL_DIR=/home/wilson/workstation/my_traffic_tf2/models/centernet_hg104_1024x1024_coco17_tpu-32/build
+MODEL_DIR=/home/wilson/workstation/my_traffic_tf2/models/faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8/build
 
 # 进入object_detection 目录
 cd /home/wilson/workstation/tensorflow/models/research/
